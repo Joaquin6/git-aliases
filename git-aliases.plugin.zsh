aabe9129a94fa0ae26d154ca490ad1c2acf9bdfc
Joaquin Briceno <joaquin_briceno@yahoo.com>
2017-08-17 17:17:45 -0700

    updated ggpull

--- git-aliases.plugin.zsh
+++ git-aliases.plugin.zsh
@@ -5,8 +5,8 @@ source $ALOCSH/git-aliases.sh
 # to remind yourself of an alias (given some part of it)
 gshowa()
 {
-  if [ -z "$1" ] then
-    echo "You must supply an argument to show aliases for"
+  if [ -z "$1" ]; then
+    echo "You must supply an argument to show aliases"
   else
     /usr/bin/grep --color=always -i -a1 $@ $ALOCSH/git-aliases.sh | grep -v '^\s*$' | less -FSRXc ;
   fi
